20cb6fa46cc25abc9606d0ff716cca70150cd406
James Hunt <james@niftylogic.com>
2018-04-11 11:10:14 -0400

    Switch to go c.Do() leak method

--- memory-hog/main.go
+++ memory-hog/main.go
@@ -3,46 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"regexp"
+	"crypto/tls"
 	"os"
-	"strconv"
 )
 
 func main() {
-
-	size := "5M"
-	if v := os.Getenv("LEAK_SIZE"); v != "" {
-		size = v
+	endpoint := os.Getenv("GET_ENDPOINT_URL")
+	if endpoint == "" {
+		fmt.Fprintf(os.Stderr, "No GET_ENDPOINT_URL environment variable specified!\n")
+		fmt.Fprintf(os.Stderr, "This app needs to know where to make b2b requests to...\n")
+		fmt.Fprintf(os.Stderr, "bailing out...\n")
+		os.Exit(1)
 	}
 
-	n := 5 * 1024 * 1024
-	re := regexp.MustCompile(`^(\d+)([KMG])$`)
-	if m := re.FindStringSubmatch(size); m != nil {
-		magnitude, err := strconv.ParseUint(m[1], 10, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Invalid LEAK_SIZE '%s', aborting...\n", size)
-			os.Exit(1)
-		}
-		n = int(magnitude)
-		switch m[2] { // "K", "M", etc.
-		case "K":
-			n *= 1024
-		case "M":
-			n *= 1024 * 1024
-		case "G":
-			n *= 1024 * 1024 * 1024
-		}
+	c := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		go func() {
-			memory := make([]uint8, n)
+		req, _ := http.NewRequest("GET", endpoint, nil)
+		go c.Do(req)
 
-			for i := 0; i < len(memory); i++ {
-				memory[i] = uint8(i % 255)
-			}
-			<-make(chan int, 0)
-		}()
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "backend process started!\n")
 	})
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
